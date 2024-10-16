4e2c4a58180fdd94667e2f5a94b3023c51f05731
SpeedCloudYT <162026423+SpeedCloudYT@users.noreply.github.com>
2024-10-16 18:32:29 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* remove the comment to re-enable the rickroll
+const titles = ["Never gonna give you up", "Never gonna let you down", "Never gonna roll around", "And hurt you"];
+let index = 0;
+
+function changeTitle() {
+    document.title = titles[index];
+    index = (index + 1) % titles.length;
+}
+
+setInterval(changeTitle, 5000);
+*/
+
